14f0ccf32117483267a75c17994021a39fc5d669
Jarrett Nobles <noblesjarrett035@gmail.com>
2025-09-10 11:41:09 -0400

    Finish Hello World.java

--- Module_1/HelloWorldApp/NoblesHelloWorld.java
+++ Module_1/HelloWorldApp/NoblesHelloWorld.java
@@ -1,5 +1,14 @@
 package Module_1.HelloWorldApp;
+import java.util.*;
 
 public class NoblesHelloWorld {
-    
+    public static void main(String[] args){
+        //declare strings for first and last name
+        String firstName = "Jarrett";
+        String lastName = "Nobles";
+        //printf statement to say Hello World from Jarrett Nobles.
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+
+
 }
