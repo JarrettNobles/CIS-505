4709a37669b5c612779506457a0ed55c0feef594
Jarrett Nobles <noblesjarrett035@gmail.com>
2025-09-10 13:15:45 -0400

    Updated

--- Module_1/FanApp/Fan.java
+++ Module_1/FanApp/Fan.java
@@ -1,4 +1,83 @@
 package Module_1.FanApp;
 
 public class Fan {
+    //1. declare constants for fan speed
+    public static final int FAST = 3;
+    public static final int MEDIUM = 2;
+    public static final int SLOW = 1;
+    public static final int STOPPED = 0;
+
+    //2-5. private int data field named speed to specify the speed of the fan and set the default to be stopped
+    private int speed = STOPPED;
+    private boolean on = false;
+    private double radius = 6;
+    private String color = "white";
+
+    //6.
+    public int getSpeed() {
+        return speed;
+
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Fan() {
+
+    }
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    /*private String speedToString(){
+        switch (speed) {
+            case SLOW: return "slow";
+            case MEDIUM: return "medium";
+            case FAST: return "fast";
+            default: return "STOPPED";
+        }
+    }*/
+
+    @Override
+    public String toString() {
+        if(on){
+            return "The fan speed is set to " + speed +
+                    " with a color of " + color +
+                    " and a radius of " + radius;
+        }
+        else{
+            return "Fan is " + color +
+                    " with a radius of "  + radius +
+                    " and the fan is off";
+        }
+    }
 }
--- Module_1/FanApp/TestFanApp.java
+++ Module_1/FanApp/TestFanApp.java
@@ -1,4 +1,19 @@
 package Module_1.FanApp;
 
 public class TestFanApp {
+    public static void main(String[] args) {
+        //default fan is STOPPED
+        Fan defaultFan = new Fan();
+        //I wanted to use all the constants I declared in the assignment, I was not sure if it is supposed to be randomized or not
+        Fan slowFan = new Fan(Fan.SLOW, true, 7, "Green");
+        Fan mediumFan = new Fan(Fan.MEDIUM, true, 8, "Blue");
+        Fan fastFan = new Fan(Fan.FAST, true, 10, "Red");
+
+        //show results
+        System.out.println(defaultFan.toString());
+        System.out.println(slowFan.toString());
+        System.out.println(mediumFan.toString());
+        System.out.println(fastFan.toString());
+
+    }
 }
