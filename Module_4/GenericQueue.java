1bcac586b520d1decde151a096563837bf3b52ac
Jarrett Nobles <noblesjarrett035@gmail.com>
2025-09-30 15:00:12 -0400

    Create que file

---
+++ Module_4/GenericQueue.java
@@ -0,0 +1,23 @@
+package Module_4;
+import java.util.*;
+import java.util.LinkedList;
+/*
+ *COURSE: CIS 505
+ * Assignment: 4.2 BowlingShopApp
+ * Author: Jarrett Nobles
+ * Due Date: 10-05-2025
+ * File: product.java
+ * Theme Focus: Inheritance and Polymorphism
+ * Description:
+ *  A simple generic queue built on LinkedList with behavior per figures 6.1 and 6.2:
+ *      -enqueue: addFirst
+ *      -dequeue: removeFirst
+ *  Notes: This is intentionally specified by the assignment (LIFO-like).
+ */
+public class GenericQueue<T> {
+    //linked list
+    private LinkedList<T> list = new LinkedList<>();
+
+    //Add an item to the front of the list (per assignment)
+    
+}
