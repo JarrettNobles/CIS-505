b0e2f6c3fc23e16f0ba41099d3fc8077a02e40ed
Jarrett Nobles <noblesjarrett035@gmail.com>
2025-09-30 14:55:59 -0400

    Create shoe.java

---
+++ Module_4/shoe.java
@@ -0,0 +1,39 @@
+package Module_4;
+import java.util.*;
+/*
+ *COURSE: CIS 505
+ * Assignment: 4.2 BowlingShopApp
+ * Author: Jarrett Nobles
+ * Due Date: 10-05-2025
+ * File: product.java
+ * Theme Focus: Inheritance and Polymorphism
+ * Description:
+ *  shoe is a product with an additional field: size.
+ *  Extends product and appends its own attribute to the display output.
+ */
+public class shoe extends product{
+    private int size;
+
+    public shoe(){
+        super();
+    }
+
+    public shoe(String code, String description, double price, int size){
+        super(code, description, price);
+        this.size = size;
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+    public void setSize(int size){
+        this.size = size;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + System.lineSeparator()
+                + "Size: " + size;
+    }//end to string
+}//end shoe class
