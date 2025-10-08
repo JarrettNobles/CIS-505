3635bf43ad5e899d86cfef0a306607b296fdfe65
Jarrett Nobles <noblesjarrett035@gmail.com>
2025-10-08 19:16:23 -0400

    Working through the Transaction Class

--- Module_5/Transaction.java
+++ Module_5/Transaction.java
@@ -12,6 +12,36 @@ import java.util.*;
  *   Includes constructors, getters, setters, and a formatted toString() method.
  */
 
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 public class Transaction {
+    //data fields
+    //date of transaction
+    private String date;
+    //description of the transaction
+    private String description;
+    //amount of the transaction
+    private double amount;
+
+    //no argument constructor
+    /*
+    Default constructor initializes the transaction with todays date,
+    an empty description, and a default amount of 0.0
+     */
+    public Transaction(){
+        SimpleDateFormat formatter = new SimpleDateFormat("MM-dd-yyyy");
+        this.date = formatter.format(new Date());
+        this.description = "";
+        this.amount = 0.0;
+    }//end constructor
+
+    /*Argument constructor that uses all 3 fields
+     * @param date the date of the transaction
+     * @param description the description of the transaction
+     * @param amount the amount of the transaction
+    */
+    public Transaction(String date, String description, double amount){
+
+    }//end constructor
+
 }
