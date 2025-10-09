dd66ebb772b29fc21a7085d06aed4ca02e04d98e
Jarrett Nobles <noblesjarrett035@gmail.com>
2025-10-08 20:08:20 -0400

    Update TransactionIO

--- Module_5/TransactionIO.java
+++ Module_5/TransactionIO.java
@@ -1,6 +1,25 @@
 package Module_5;
+//imports
 import java.util.*;
-
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+ * Class: CIS 505
+ * Module: 5
+ * File: TransactionIO.java
+ * Author: Jarrett Nobles
+ * Date: 10/08/2025
+ * Description:
+ *   Handles file input and output operations for Transaction objects.
+ *   Provides methods to write multiple transactions to a text file
+ *   and to read all stored transactions back into an ArrayList.
+ */
 public class TransactionIO {
+    //data fields
+    private static final String FILE_NAME = "expenses.txt";
+    private static final File file = new File(FILE_NAME);
 
 }//end class
